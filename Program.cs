01f8a08e74871d52027809d196b2fdc171060259
marcelldac <dacmarcell@gmail.com>
2023-10-08 18:46:11 -0300

    feat: adiciona menu

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace TextEditor{
+    class Program{
+        static void Main(string[] args){
+            Menu();
+        }
+
+        static void Menu(){
+            Console.Clear();
+
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1 - Abrir arquivo.");
+            Console.WriteLine("2 - Criar novo arquivo.");
+            Console.WriteLine("0 - Sair arquivo.");
+            
+            short opcao = short.Parse(Console.ReadLine());
+
+            switch(opcao){
+                case 0: Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Abrir(){
+
+        }
+        
+        static void Editar(){
+
+        }
+    }
+}
