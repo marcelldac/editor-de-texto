2cdafd992607b1074b9660748e39f7ce6acc250f
marcelldac <dacmarcell@gmail.com>
2023-10-08 23:43:58 -0300

    chore: editorconfig

--- Program.cs
+++ Program.cs
@@ -1,35 +1,43 @@
 ﻿using System;
 
-namespace TextEditor{
-    class Program{
-        static void Main(string[] args){
-            Menu();
-        }
-
-        static void Menu(){
-            Console.Clear();
-
-            Console.WriteLine("O que você deseja fazer?");
-            Console.WriteLine("1 - Abrir arquivo.");
-            Console.WriteLine("2 - Criar novo arquivo.");
-            Console.WriteLine("0 - Sair arquivo.");
-            
-            short opcao = short.Parse(Console.ReadLine());
-
-            switch(opcao){
-                case 0: Environment.Exit(0); break;
-                case 1: Abrir(); break;
-                case 2: Editar(); break;
-                default: Menu(); break;
-            }
-        }
-
-        static void Abrir(){
-
-        }
-        
-        static void Editar(){
-
-        }
+namespace TextEditor
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Menu();
     }
+
+    static void Menu()
+    {
+      Console.Clear();
+
+      Console.WriteLine("O que você deseja fazer?");
+      Console.WriteLine("1 - Abrir arquivo.");
+      Console.WriteLine("2 - Criar novo arquivo.");
+      Console.WriteLine("0 - Sair arquivo.");
+
+      short opcao = short.Parse(Console.ReadLine());
+
+      switch (opcao)
+      {
+        case 0: Environment.Exit(0); break;
+        case 1: Abrir(); break;
+        case 2: Editar(); break;
+        default: Menu(); break;
+      }
+    }
+
+    static void Abrir()
+    {
+
+    }
+
+    static void Editar()
+    {
+      Console.Clear();
+      Console.WriteLine();
+    }
+  }
 }
